b8b92a559547d0f03fa5f882aa512292bd405a39
snagaoka <shannonnagaoka@gmail.com>
2014-08-09 11:31:43 -1000

    initial commit

--- challenge_4.js
+++ challenge_4.js
@@ -8,16 +8,37 @@
  *  and the palindromeNumber itself.
  */
 exports.largestPalindromeProduct = function(multiplicands, digits){
-  var factor_0 = 0;
-  var factor_1 = 0;
-  var palindromeNumber = 0;
-  // do your work here
+	var factor_0 = 0;	
+	var factor_1 = 0;
+	var palindromeNumber = 0;
+	// do your work here
 
+
+	// 
+	for (factor_0 = 100; factor_0 < 1000; factor_0++) {
+		for (factor_1 = 100; factor_1 < 1000; factor_1++) {
+			var product = factor_0 * factor_1;
+			var numberString = product.toString();
+			if(numberString === numberString.split('').reverse().join('')){
+				console.log(numberString + ' is a palindrome');
+			};
+
+			// Need to sort numberString
+
+
+			// return last item in array
+
+
+			return largestPalindrome;
+		};
+	};
 
   
-  return {
-    factor_0 : factor_0,
-    factor_1 : factor_1,
-    palindromeNumber : palindromeNumber
-  };
+
+
+	return {
+		factor_0 : factor_0,
+		factor_1 : factor_1,
+		palindromeNumber : palindromeNumber
+	};
 };
